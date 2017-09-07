ab8d724a1f5f93669e8147294f62932220dad93e
david mallon <mallond@gmail.com>
2017-09-07 16:38:22 -0500

    clean up

--- pipe1.js
+++ pipe1.js
@@ -4,7 +4,9 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         var node = this;
         this.on('input', function (msg) {
-            var tax = msg.tax;
+            msg.payload.total = msg.payload.total>0?msg.payload.total:msg.payload.amount;
+            msg.payload.steps.push('Pipe 1 apply Tax: '+msg.payload.tax +' to: ' +msg.payload.total +' = '+msg.payload.total*msg.payload.tax);
+            msg.payload.total = msg.payload.total*msg.payload.tax;
             node.send(msg);
         });
     }
